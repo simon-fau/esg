0c504a74cef9f987a588fe30e3346ddc278ca04f
AndreeS98 <andreepascal@web.de>
2024-03-21 13:06:00 +0100

    neu

--- pages/Potentielle_Nachhaltigkeitspunkte.py
+++ pages/Potentielle_Nachhaltigkeitspunkte.py
@@ -1,99 +1,93 @@
 import streamlit as st
 import pandas as pd
 
+def convert_df_to_csv(df):
+    # Konvertiere ein DataFrame in ein CSV-Objekt, bereit zum Herunterladen
+    return df.to_csv(index=False).encode('utf-8')
+
 def display_page():
+
     # Initialisiere den Session State für die hochgeladenen Dateien, falls noch nicht geschehen
     if 'uploaded_files' not in st.session_state:
         st.session_state['uploaded_files'] = None
 
-    col1, col2 = st.columns([3, 1])
+    # Initialisiere den Session State für das DataFrame, falls noch nicht geschehen
+    if 'dataf' not in st.session_state:
+        # Initialisiere das DataFrame mit den gegebenen Inhalten
+        st.session_state['dataf'] = pd.DataFrame(
+            [
+                ["E1", "Klimawandel", "Anpassung an den Klimawandel", ""],
+                ["E1", "Klimawandel", "Eindämmung des Klimawandels", ""],
+                ["E1", "Klimawandel", "Energie", ""],
+                ["E2", "Verschmutzung", "Luftverschmutzung", ""],
+                ["E2", "Verschmutzung", "Wasserverschmutzung", ""],
+                ["E2", "Verschmutzung", "Bodenverschmutzung", ""],
+                ["E2", "Verschmutzung", "Verschmutzung lebender Organismen und Nahrungsressourcen", ""],
+                ["E2", "Verschmutzung", "Verschmutzung: Bedenkliche Stoffe", ""],
+                ["E2", "Verschmutzung", "Verschmutzung: Sehr bedenkliche Stoffe", ""],
+                ["E3", "Wasser- und Meeresressourcen", "Wasserentnahmen", ""],
+                ["E3", "Wasser- und Meeresressourcen", "Wasserverbrauch", ""],
+                ["E3", "Wasser- und Meeresressourcen", "Wassernutzung", ""],
+                ["E3", "Wasser- und Meeresressourcen", "Wassereinleitungen in Gewässer und in die Ozeane", ""],
+                ["E3", "Wasser- und Meeresressourcen", "Verschlechterung der Wasser-/Meereshabitate und Intensität des Einflusses auf die Meeresressourcen", ""],
+                ["E4", "Biodiversität und Ökosysteme", "Verlust der biologischen Vielfalt", ""],
+                ["E4", "Biodiversität und Ökosysteme", "Auswirkungen auf den Zustand der Arten", ""],
+                ["E4", "Biodiversität und Ökosysteme", "Auswirkungen auf und Abhängigkeiten von Ökosystemleistungen", ""],
+                ["E5", "Kreislaufwirtschaft", "Ressourcenzuflüsse, einschließlich Ressourcennutzung", ""],
+                ["E5", "Kreislaufwirtschaft", "Ressourcenabflüsse in Bezug auf Produkte und Dienstleistungen", ""],
+                ["E5", "Kreislaufwirtschaft", "Abfall", ""],
+
+            ],
+            columns=["ESRS", "Nachhaltigkeitsaspekt", "Themen", "Unterthemen"]
+        )
+
+    col1, col2 = st.columns([2, 1])  # Ändern der Spaltenverhältnisse für eine breitere Darstellung des DataFrames
     with col1:
-        uploaded_files = st.file_uploader("Wählen Sie ein Dokument aus", accept_multiple_files=True, key="file_uploader")
+        if 'dataf' not in st.session_state:
+            st.session_state['dataf'] = pd.DataFrame()  # Initialisiere das DataFrame mit leeren Daten
+        st.session_state['dataf'] = st.data_editor(st.session_state['dataf'], num_rows="dynamic", height=700)
     
     with col2:
+        uploaded_files = st.file_uploader("Wählen Sie ein Dokument aus", accept_multiple_files=True, key="file_uploader")
         if st.button("Analysieren", key="analyze_button"):
-            # Speichere die hochgeladenen Dateien im Session State, wenn der Button gedrückt wurde
             st.session_state['uploaded_files'] = uploaded_files
-    
-    # Verwende die hochgeladenen Dateien aus dem Session State, wenn vorhanden
-    if st.session_state['uploaded_files'] is not None:
-        for uploaded_file in st.session_state['uploaded_files']:
-            bytes_data = uploaded_file.read()
-            # Zeige den Dateinamen und den Inhalt hier an
-            st.write("Dateiname:", uploaded_file.name)
-            # Für eine bessere Handhabung großer Dateien oder binärer Daten, könnte man hier weitere Logik implementieren
-            st.write(bytes_data)
 
-    
+        # Verwende die hochgeladenen Dateien aus dem Session State, wenn vorhanden
+        if st.session_state['uploaded_files'] is not None:
+            for uploaded_file in st.session_state['uploaded_files']:
+                bytes_data = uploaded_file.read()
+                st.write("Dateiname:", uploaded_file.name)
+                st.write(bytes_data)
 
-    html_table = """
-        <table>
-            <tr>
-                <th>ESRS</th>
-                <th>Topic</th>
-                <th>Sub Topic</th>
-                <th>Sub-sub-Topic</th>
-            </tr>
-            <tr>
-                <td rowspan="3">E1</td>
-                <td rowspan="3">Climate Change</td>
-                <td>Climate change adaptation</td>
-                <td>Detail 1 für Climate change adaptation</td>
-            </tr>
-            <tr>
-                <td>Climate change mitigation</td>
-                <td>Detail 2 für Climate change mitigation</td>
-            </tr>
-            <tr>
-                <td>Energy</td>
-                <td>Detail 3 für Energy</td>
-            </tr>
-            <tr>
-                <td rowspan="5">E2</td>
-                <td rowspan="5">Pollution</td>
-                <td>Air pollution</td>
-                <td>Detail 1 für Air pollution</td>
-            <tr>
-                <td>Water pollution</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Soil pollution</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Pollution of living organisms and food resources</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Pollution: Substances of concern</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td rowspan="5">E3</td>
-                <td rowspan="5">Water and marine resources</td>
-                <td>Water withdrawals</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Water consumption</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Water use</td>  
-                <td>Detail 1 für Air pollution</td>  
-            </tr>
-            <tr>
-                <td>Water discharges to water bodies and oceans</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr>
-            <tr>
-                <td>Degradation of aquatic/marine habitats and intensity of impact on marine resources</td>
-                <td>Detail 1 für Air pollution</td>
-            </tr> 
-    </table>
-    """
-    # Verwende HTML, um die Inhalte anzuzeigen
-    st.markdown(html_table, unsafe_allow_html=True)
-  
-   
+        # Trennlinie zwischen Datei-Uploader und Selectboxen
+        st.markdown("---")  # Markdown für eine horizontale Linie
+
+        #Erstelle Selectboxen für die Eingabe neuer Daten
+        esrs = st.selectbox("ESRS", ["E1", "E2", "E3", "E4", "E5"], key="esrs_select")
+        nachhaltigkeitsaspekt = st.selectbox("Nachhaltigkeitsaspekt", ["Klimawandel", "Verschmutzung", "Wasser- und Meeresressourcen",  "Biodiversität und Ökosysteme", "Kreislaufwirtschaft"], key="nachhaltigkeitsaspekt_select")
+        themen = st.text_input("Thema", key="themen_input")
+        unterpunkte = st.text_input("Unterthema", key="unterthemen_input")  
+
+        # Button, um die Auswahl aus den Selectboxen zum DataFrame hinzuzufügen
+        if st.button("Hinzufügen"):
+            #Füge eine neue Zeile zum DataFrame hinzu
+            neue_zeile = {
+                "ESRS": esrs,
+                "Nachhaltigkeitsaspekt": nachhaltigkeitsaspekt,
+                "Themen": themen,
+                "Unterthemen": unterpunkte
+            }
+            neue_zeile = pd.DataFrame([[esrs, nachhaltigkeitsaspekt, themen, unterpunkte]], columns=st.session_state['dataf'].columns)
+            st.session_state['dataf'] = pd.concat([st.session_state['dataf'], neue_zeile], ignore_index=True)
+
+        # Trennlinie zwischen Datei-Uploader und Selectboxen
+        st.markdown("---")  # Markdown für eine horizontale Linie
+        
+        # Download-Button
+        csv = convert_df_to_csv(st.session_state['dataf'])
+        st.download_button(
+            label="Tabelle herunterladen",
+            data=csv,
+            file_name='dataframe.csv',
+            mime='text/csv',
+        )
