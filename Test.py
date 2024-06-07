08a8ebb69a49406ac8f0ce618df06dbaf72e7c30
AndreeS98 <andreepascal@web.de>
2024-06-07 16:10:51 +0200

    a

--- Test.py
+++ Test.py
@@ -8,26 +8,35 @@ class YesNoSelection:
         self.initialize_state()
 
     def initialize_state(self):
-        options = {'Ja': False, 'Nein': False}
+        options = {'Ja': False, 'Nein': False, 'Durst_Ja': False, 'Durst_Nein': False}
         if 'yes_no_selection' not in st.session_state:
             st.session_state['yes_no_selection'] = options
 
     def save_session_state(self):
-        with open('session_state.pkl', 'wb') as f:
+        with open('a.pkl', 'wb') as f:
             pickle.dump(st.session_state['yes_no_selection'], f)
 
     def load_session_state(self):
-        if os.path.exists('session_state.pkl'):
-            with open('session_state.pkl', 'rb') as f:
+        if os.path.exists('a.pkl'):
+            with open('a.pkl', 'rb') as f:
                 st.session_state['yes_no_selection'] = pickle.load(f)
 
     def create_options_row(self):
-        row = st.columns([1, 0.5, 0.5])
-        row[0].write("Ihre Auswahl:")
-        ja_checked = row[1].checkbox("Ja", value=st.session_state['yes_no_selection']['Ja'])
-        nein_checked = row[2].checkbox("Nein", value=st.session_state['yes_no_selection']['Nein'])
+        hunger_row = st.columns([1, 0.5, 0.5])
+        hunger_row[0].write("Hunger:")
+        ja_checked = hunger_row[1].checkbox("Ja", value=st.session_state['yes_no_selection'].get('Hunger_Ja', False), key="hunger_ja")
+        nein_checked = hunger_row[2].checkbox("Nein", value=st.session_state['yes_no_selection'].get('Hunger_Nein', False), key="hunger_nein")
         
-        st.session_state['yes_no_selection'] = {'Ja': ja_checked, 'Nein': nein_checked}
+        durst_row = st.columns([1, 0.5, 0.5])
+        durst_row[0].write("Durst:")
+        durst_ja_checked = durst_row[1].checkbox("Ja", value=st.session_state['yes_no_selection'].get('Durst_Ja', False), key="durst_ja")
+        durst_nein_checked = durst_row[2].checkbox("Nein", value=st.session_state['yes_no_selection'].get('Durst_Nein', False), key="durst_nein")
+        
+        # Aktualisieren Sie die Zustände im st.session_state korrekt, ohne Schlüssel zu überschreiben
+        st.session_state['yes_no_selection']['Hunger_Ja'] = ja_checked
+        st.session_state['yes_no_selection']['Hunger_Nein'] = nein_checked
+        st.session_state['yes_no_selection']['Durst_Ja'] = durst_ja_checked
+        st.session_state['yes_no_selection']['Durst_Nein'] = durst_nein_checked
 
     def display_selection(self):
         st.header('Ja oder Nein Auswahl')
