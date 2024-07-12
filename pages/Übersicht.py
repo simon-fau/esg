e7cf88320db8de54cbbea2ecb216255cb82ffeb5
AndreeS98 <andreepascal@web.de>
2024-07-12 13:24:16 +0200

    Übersicht mit ploty Graphik

--- pages/Übersicht.py
+++ pages/Übersicht.py
@@ -3,6 +3,7 @@ from pages.Stakeholder_Management import display_stakeholder_ranking_and_network
 from pages.Externe_Nachhaltigkeitspunkte import calculate_class_size, calculate_selected_rows, display_aggrid
 from pages.Longlist import merge_dataframes, bewertung_Uebersicht
 import plotly.express as px
+import plotly.graph_objects as go
 
 def display_stakeholder_table():
     class_size = calculate_class_size(st.session_state.stakeholder_punkte_df)
@@ -22,7 +23,6 @@ def companies_in_stakeholder_table():
         for index, row in st.session_state.company_names.iterrows():
             st.markdown(f"- {row['Company Name']}")
 
-# Funktion zum Erstellen der Grafik
 def auswirkungsbezogene_graphik():
     if 'selected_columns' in st.session_state and len(st.session_state['selected_columns']) > 0:
         df = st.session_state.selected_columns
@@ -30,11 +30,11 @@ def auswirkungsbezogene_graphik():
         # Farbcodierung basierend auf der Untersuchung der Zeichenkette in "Score Auswirkung"
         def determine_color(impact):
             if 'Positive Auswirkung' in impact:
-                return 'green'
+                return 'Positive Auswirkung'
             elif 'Negative Auswirkung' in impact:
-                return 'red'
+                return 'Negative Auswirkung'
             else:
-                return 'grey'
+                return 'Neutral'
 
         df['color'] = df['Auswirkung'].apply(determine_color)
 
@@ -47,13 +47,22 @@ def auswirkungsbezogene_graphik():
         # Erstellen der Scatter-Graphik
         fig = px.scatter(df, x='Score Finanzen', y='Score Auswirkung', color='color',
                          size='Stakeholder Wichtigkeit',  # Größe der Punkte basierend auf Stakeholder Wichtigkeit
-                         color_discrete_map={'green': 'green', 'red': 'red', 'grey': 'grey'},
+                         color_discrete_map={'Positive Auswirkung': 'green', 'Negative Auswirkung': 'red', 'Neutral': 'grey'},
                          labels={'Score Finanzen': 'Score Finanzen', 'Score Auswirkung': 'Score Auswirkung'},
                          title='Auswirkungsbezogene Grafik',
                          range_x=[0, 1000],  # X-Achsenbereich von 0 bis 1000
                          range_y=[0, 1000],  # Y-Achsenbereich von 0 bis 1000
                          width=1000,  # Breite des Charts
-                         height=800)  # Höhe des Charts
+                         height=800,  # Höhe des Charts
+                         hover_data={
+                             'Thema': True,
+                             'Unterthema': True,
+                             'Unter-Unterthema': True,
+                             'Score Auswirkung': True,
+                             'Score Finanzen': True,
+                             'Stakeholder Wichtigkeit': True,
+                             'color': False  # Verhindert das Anzeigen der Farbe im Tooltip
+                         })
 
         # Hinzufügen des Rasters und Festlegen der Achsenintervalle
         fig.update_layout(
@@ -65,8 +74,18 @@ def auswirkungsbezogene_graphik():
                 gridcolor='lightgrey',
                 gridwidth=1,
                 griddash='dash',
-                zeroline=False,
-                layer='below traces'
+                zeroline=True,
+                layer='above traces',
+                side='bottom'
+            ),
+            xaxis2=dict(
+                tickmode='linear',
+                tick0=0,
+                dtick=100,
+                showgrid=False,
+                zeroline=True,
+                overlaying='x',
+                side='top'
             ),
             yaxis=dict(
                 tickmode='linear',
@@ -76,16 +95,23 @@ def auswirkungsbezogene_graphik():
                 gridcolor='lightgrey',
                 gridwidth=1,
                 griddash='dash',
-                zeroline=False,
-                layer='below traces'
-            )
+                zeroline=True,
+                layer='above traces'
+            ),
+            legend_title_text='Auswirkungen',
+            staticPlot=True  # Disable interactivity
         )
 
         # Hinzufügen zusätzlicher Rasterlinien alle 50 Einheiten
-        fig.update_xaxes(showline=True, gridwidth=1, gridcolor='lightgrey', tick0=0, dtick=50)
-        fig.update_yaxes(showline=True, gridwidth=1, gridcolor='lightgrey', tick0=0, dtick=50)
+        fig.update_xaxes(showline=True, gridwidth=1, gridcolor='lightgrey', tick0=0, dtick=100)
+        fig.update_yaxes(showline=True, gridwidth=1, gridcolor='lightgrey', tick0=0, dtick=100)
 
-        st.plotly_chart(fig)
+        # Regler zum Bewegen der Linie hinzufügen
+        line_position = st.slider('Linienposition', min_value=0, max_value=1000, value=400, step=10)
+        
+        fig.add_trace(go.Scatter(x=[0, line_position], y=[line_position, 0], mode='lines', name='Grenzlinie Stakeholder Wichtigkeit', line=dict(color='red')))
+
+        st.plotly_chart(fig, use_container_width=True)
 
 def display_page():
     tab1, tab2 = st.tabs(["Stakeholder", "Longlist"])
@@ -103,4 +129,3 @@ def display_page():
         st.write("Selected Columns:", st.session_state.get('selected_columns', 'Keine Spalten ausgewählt'))
         auswirkungsbezogene_graphik()
         
-    
