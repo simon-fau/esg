d9aafb514a5bc85e69263c9092e41b6df0e791b9
AndreeS98 <andreepascal@web.de>
2024-08-02 10:35:02 +0200

    zj

--- pages/Themenspezifische_ESRS.py
+++ pages/Themenspezifische_ESRS.py
@@ -1,38 +1,11 @@
 import streamlit as st
 import pickle
 import os
+import time
 
 def initialize_state():
-    options = {
-        'Relevance_Klimawandel': None,
-        'Relevance_Klimawandel_2': None,
-        'Relevance_Luftverschmutzung': None,
-        'Relevance_Wasserverschmutzung': None,
-        'Relevance_Bodenverschmutzung': None,
-        'Relevance_Verschmutzung_von_lebenden_Organismen_und_Nahrungsressourcen': None,
-        'Relevance_Besorgniserregende_Stoffe': None,
-        'Relevance_Besonders_besorgniserregende_Stoffe': None,
-        'Relevance_Mikroplastik': None,
-        'Relevance_Wasserverbrauch': None,
-        'Relevance_Wasserentnahme': None,
-        'Relevance_Ableitung_von_Wasser': None,
-        'Relevance_Ableitung_von_Wasser_in_die_Ozeane': None,
-        'Relevance_Gewinnung_und_Nutzung_von_Meeresressourcen': None,
-        'Relevance_Klimawandel_E4': None,
-        'Relevance_Land-_Süßwasser-_und_Meeresnutzungsänderungen': None,
-        'Relevance_Direkte_Ausbeutung': None,
-        'Relevance_Invasive_gebietsfremde_Arten': None,
-        'Relevance_Umweltverschmutzung_E4': None,
-        'Relevance_Sonstige': None,
-        'Relevance_Populationsgröße_von_Arten': None,
-        'Relevance_Globales_Ausrottungsrisiko_von_Arten': None,
-        'Relevance_Landdegradation': None,
-        'Relevance_Wüstenbildung': None,
-        'Relevance_Bodenversiegelung': None,
-        'Relevance_Auswirkungen_und_Abhängigkeiten_von_Ökosystemdienstleistungen': None
-    }
     if 'yes_no_selection' not in st.session_state:
-        st.session_state['yes_no_selection'] = options
+        st.session_state['yes_no_selection'] 
 
 def save_session_state():
     with open('session_states_top_down.pkl', 'wb') as f:
@@ -55,12 +28,25 @@ def Text():
 def display_section(topics, section_key):
     current_selection = {}
 
+    # CSS to increase the horizontal spacing between radio buttons
+    st.markdown(
+        """
+        <style>
+        .stRadio > div {
+            display: flex;
+            gap: 155px; /* Adjust the value as needed */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     for topic, key in topics:
-        cols = st.columns([4, 2])
+        cols = st.columns([3.35, 1])
         cols[0].write(f"{topic}:")
         radio_key = f"Relevance_{key}_{section_key}"
         selected_option = st.session_state['yes_no_selection'].get(radio_key, None)
-        option = cols[1].radio("", options=["Ja", "Nein"], index=0 if selected_option == "Relevant für Bewertung" else 1 if selected_option == "Unrelevant für Bewertung" else None, key=radio_key, label_visibility='collapsed', horizontal=True)
+        option = cols[1].radio("", options=[" ", " "],index=0 if selected_option == "Relevant für Bewertung" else 1 if selected_option == "Unrelevant für Bewertung" else None, key=radio_key, label_visibility='collapsed', horizontal=True)
         current_selection[radio_key] = option
 
     st.session_state['yes_no_selection'] = {**st.session_state['yes_no_selection'], **current_selection}
@@ -72,11 +58,11 @@ def display_complex_section(sections, section_key):
         st.markdown(f"**{title}**")
         current_selection = {}
         for topic, key in topics:
-            cols = st.columns([4, 2])
+            cols = st.columns([3.35, 1])
             cols[0].write(f"{topic}:")
             radio_key = f"Relevance_{key}_{section_key}"
             selected_option = st.session_state['yes_no_selection'].get(radio_key, None)
-            option = cols[1].radio("", options=["Ja", "Nein"], index=0 if selected_option == "Relevant für Bewertung" else 1 if selected_option == "Unrelevant für Bewertung" else None, key=radio_key, label_visibility='collapsed', horizontal=True)
+            option = cols[1].radio("", options=[" ", " "], index=0 if selected_option == "Relevant für Bewertung" else 1 if selected_option == "Unrelevant für Bewertung" else None, key=radio_key, label_visibility='collapsed', horizontal=True)
             current_selection[radio_key] = option
         return current_selection
 
@@ -90,14 +76,27 @@ def display_complex_section(sections, section_key):
     return True
 
 def display_save_button(section_name):
-    col1, col2 = st.columns([4, 2])
+    Placeholder()
+    col1, col2 = st.columns([5, 1.5])
     with col2:
-        st.write("")  # Platzhalter
-        st.write("")    
-        st.write("")
+        Placeholder()
+        # CSS für rechtsbündige Anzeige des Buttons
+        st.markdown(
+            """
+            <style>
+            .stButton button {
+                float: right;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
         if st.button(f"💾 Auswahl speichern", key=f'Button_{section_name}'):
-            st.success(f"Auswahl erfolgreich gespeichert!")
+            success_placeholder = st.empty()
+            success_placeholder.success(f"Auswahl erfolgreich gespeichert!")
             save_session_state()
+            time.sleep(3)
+            success_placeholder.empty()
                 
 def display_E1_Klimawandel():
     topics = [("Anpassung an Klimawandel", "Anpassung_an_den_Klimawandel"), ("Klimaschutz", "Klimaschutz"), ("Energie", "Energie")]
@@ -154,6 +153,7 @@ def Placeholder():
     st.write("")
 
 def display_page():
+
     load_session_state()
     initialize_state()
     col1, col2 = st.columns([4, 1])
@@ -167,41 +167,58 @@ def display_page():
     tabs = st.tabs(["Klimawandel", "Umweltverschmutzung", "Wasser- und Meeressourcen", "Biodiversität"])
     
     with tabs[0]:
-        col1, col2 = st.columns([1, 4])
+        col1, col2, col3, col4 = st.columns([4, 1, 1, 1])
         with col1:
-            st.subheader("Klimawandel - Sind die folgenden Themen relevant für Ihre Bewertung?")
+            st.subheader("Klimawandel")
         with col2:
             pass
+        with col3:
+            st.write("**Relevant für Bewertung**")
+        with col4:
+            st.write("**Unrelevant für Bewertung**")
+            
         Placeholder()
         display_E1_Klimawandel()
     
     with tabs[1]:
-        col1, col2 = st.columns([2, 1.5])
+        col1, col2, col3, col4 = st.columns([4, 1, 1, 1])
         with col1:
             st.subheader("Umweltverschmutzung")
         with col2:
-            Placeholder()
-            st.write("**Sind die folgenden Themen relevant für Ihre Bewertung?**")
+            pass
+        with col3:
+            st.write("**Relevant für Bewertung**")
+        with col4:
+            st.write("**Unrelevant für Bewertung**")
+
         Placeholder()
         display_E2_Umweltverschmutzung()
     
     with tabs[2]:
-        col1, col2 = st.columns([2, 1.5])
+        col1, col2, col3, col4 = st.columns([4, 1, 1, 1])
         with col1:
             st.subheader("Meeres- und Wasserressourcen")
         with col2:
-            Placeholder()
-            st.write("**Sind die folgenden Themen relevant für Ihre Bewertung?**")
+            pass
+        with col3:
+            st.write("**Relevant für Bewertung**")
+        with col4:
+            st.write("**Unrelevant für Bewertung**")
+
         Placeholder()
         display_E3_Wasser_und_Meeresressourcen()
     
     with tabs[3]:
-        col1, col2 = st.columns([2, 1.5])
+        col1, col2, col3, col4 = st.columns([4, 1, 1, 1])
         with col1:
             st.subheader("Biodiversität")
         with col2:
-            Placeholder()
-            st.write("**Sind die folgenden Themen relevant für Ihre Bewertung?**")
+            pass
+        with col3:
+            st.write("**Relevant für Bewertung**")
+        with col4:
+            st.write("**Unrelevant für Bewertung**")
+            
         Placeholder()
         display_E4_Biodiversität()
 
